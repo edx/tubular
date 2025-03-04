4342c1a7aec6597b8087e4cb94478cfd1510745c
Bernard Szabo <bszabo@edx.org>
2025-03-04 09:47:25 -0500

    feat: postman tested regex changes

--- tubular/scripts/dd_synthetic_tests.py
+++ tubular/scripts/dd_synthetic_tests.py
@@ -134,7 +134,7 @@ class DatadogClient:
         x = "(\w+)\.edx.org|1.stage.edx.org"
         json_request_body = {"tests": [{"public_id": test.public_id,
                                         "startUrl" : test.start_url,
-                                        "resourceUrlSubstitutionRegexes": self._map_environment_resources(test.env)
+                                        "resourceUrlSubstitutionRegexes": [self._map_environment_resources(test.env)]
                                        }
                                        for test in self.tests_by_public_id.values()]}
         logging.info(f'Trigger request body: {json_request_body}')
@@ -146,8 +146,7 @@ class DatadogClient:
     def _map_environment_resources(self, env):
         if not env or env == 'prod' or env == 'stage':
             logging.info("***** Relying on identity transformation for environment resources ***** ")
-            return [{'pattern': '(?P<identity>.*)',
-                     'substitution': '{{identity}}'}]  # No change
+            return '(?P<identity>.*)|{{identity}}'   # No change
         elif env == 'stage':
             return r'"(.+)\.edx.org|$1.stage.edx.org"'
         else:
