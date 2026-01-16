54c3f3f49e025b3f3293366b362630eaf4e824e0
ktyagiapphelix2u <ktyagi-apphelix@2u.com>
2026-01-16 04:46:38 +0000

    fix: redacting user retirement data in lms

--- tubular/edx_api.py
+++ tubular/edx_api.py
@@ -361,13 +361,13 @@ class LmsApi(BaseApiClient):
         return self._request('POST', api_url)
 
     @_retry_lms_api()
-    def bulk_cleanup_retirements(self, usernames, jenkins_run_id=None):
+    def bulk_cleanup_retirements(self, usernames, run_id=None):
         """
         Deletes the retirements for all given usernames
         """
         data = {'usernames': usernames}
-        if jenkins_run_id:
-            data['jenkins_run_id'] = jenkins_run_id
+        if run_id:
+            data['run_id'] = run_id
         api_url = self.get_api_url('api/user/v1/accounts/retirement_cleanup')
         return self._request('POST', api_url, json=data)
 
