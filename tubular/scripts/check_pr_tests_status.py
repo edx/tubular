720e7706b345fc238b1429984608a13a1a208650
Akanshu Aich <aaich-apphelix@2u.com>
2026-03-06 11:25:22 +0000

    fix: Default to checking ALL checks, not just required ones

--- tubular/scripts/check_pr_tests_status.py
+++ tubular/scripts/check_pr_tests_status.py
@@ -59,10 +59,9 @@ LOG = logging.getLogger(__name__)
     default=sys.stdout
 )
 @click.option(
-    '--all-checks',
-    help="Check all validation contexts, whehther it is required or not",
-    is_flag=True,
-    default=False
+    '--all-checks/--required-checks-only',
+    help="Check all validation contexts, or only required ones.",
+    default=True
 )
 @click.option(
     '--exclude-contexts',
